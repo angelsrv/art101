6a127e1b96ce3485242a263a245641ade2f16479
angelsrv <angelsrv22@gmail.com>
2025-10-21 18:51:00 -0700

    new files

--- Exercises/ex04.js
+++ Exercises/ex04.js
@@ -1,13 +1,28 @@
-let count = 0;
-let colors= ["Orchid", "Coral", "HotPink", "Plum"];
+var count = 0;
+let colorCount= 0;
+var colors= ["Orchid", "Coral", "HotPink", "Plum"];
+
 
 $("#needy-button").click( function() {
- 
-   $("#needy-button").html(" Clicks: " + count + " Color: " + colors[count] );
 
-   $("body").css("background-color", color[count]);
+   if ( count < 5) {mood = " fresh and happy"}
+else if ( (count >=5) && (count <10) ) {mood=" keep pushing"; }
+else {mood = " so tired"; }
+ 
+   $("#needy-button").html(" Clicks: " + count + " Color: " + colors[colorCount] + mood);
+
+   $("body").css("background-color", colors[colorCount]) ;
+
+   
+
+   $("body").append("<img width=50 src='images/orchid.webp'>");
 
 count = count + 1;
+colorCount = colorCount + 1;
+
+if ( colorCount == 4 ) { colorCount=0; } //new line also == <--- means something from different from =
+
+
 
 });
 
---
+++ Exercises/lab04.js
@@ -0,0 +1,17 @@
+// declaring an array with name myCommutes
+let myCommutes = ["metro bus", "walking", 
+    "Melody car", "friends"];
+     
+    // declaring an object with name myFavouriteCommute
+let myFavouriteCommute = {
+    type: "Melody Car",
+    route: "coastalCampusToKresge",
+    color: "gray",
+    hasCupHolders: true,
+    drivers: ["Melody", "Angel"],
+};
+
+let megaSentence;
+
+megaSentence = "<p>My two top commutes from the array are: " 
++ myCommutes[0] + ", " + myCommutes[5] + "</p>";
