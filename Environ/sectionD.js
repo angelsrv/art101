41700b598deb274458d3762059be9783a6aadb94
angelsrv <angelsrv22@gmail.com>
2026-05-25 20:39:08 -0700

    updates
    updates

---
+++ Environ/sectionD.js
@@ -0,0 +1,26 @@
+//declaring variables strings are qoutes "miranda" & "Angel"
+//integer are just the number itself no qoutes: 5 
+
+let person_1 = "Miranda" ;
+let person_2 = "Angel" ;
+let money_1 = 5 ;
+let money_2 = 5 ; 
+
+function coffee_purchase (person_1, money_1)
+{
+    //telling computer to decide
+    if (money_1 > 5 );
+        {
+            print (person_1 + "Gets coffee!!!");
+        }
+    if (money_1 == 5);
+        {
+            print (person_1 + "Gets coffee!!! watch yo savings tho");
+        }
+    if (money_1 > 5 && money_1 ==5 );
+        {
+            print (person_1 + "no coffee");
+        }    
+}
+
+coffee_purchase (person_1, money_1)
